14a42ee2085297c3efbb7ff2ac755fc750c7c561
Sander Weiss <weisssander@gmail.com>
2023-05-21 16:17:57 -0400

    boilerplate

---
+++ app/head.tsx
@@ -0,0 +1,5 @@
+export default function Head() {
+  return (
+    <div className="text-5xl">Hi</div>
+  )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+"use client"
+import "../styles/globals.css"
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <h1>hi</h1>
+  )
+}
